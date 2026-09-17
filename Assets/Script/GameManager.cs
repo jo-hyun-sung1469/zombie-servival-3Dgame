250c94349282c60343b89ff4d98cc21c76cfadb7
jo-hyun-sung1469 <hunsung1469@gmail.com>
2026-09-17 11:51:39 +0900

    fix: GameManager의 기능 삭제
    #2

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,11 +1,8 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private Camera firstPersonCamera;
-    [SerializeField] private Camera loginCamera;
-    [SerializeField] private GameObject ingameUI;
-    [SerializeField] private GameObject loginUI;
     private LoginResponse loginResponse;//LoginResponse에 있는 AssessToken을 저장&사용하기 위한 변수
     public LoginResponse LoginResponse
     {
@@ -25,33 +22,5 @@ public class GameManager : MonoBehaviour
 
         _instance = this;
         DontDestroyOnLoad(gameObject);
-    }
-    private void Start()
-    {
-        ShowLogin();
-    }
-
-    public void ShowLogin()
-    {
-        firstPersonCamera.gameObject.SetActive(false);
-        loginCamera.gameObject.SetActive(true);
-
-        ingameUI.SetActive(false);
-        loginUI.SetActive(true);
-
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-    }
-
-    public void StartGame()
-    {
-        loginCamera.gameObject.SetActive(false);
-        firstPersonCamera.gameObject.SetActive(true);
-
-        loginUI.SetActive(false);
-        ingameUI.SetActive(true);
-
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
     }
 }
